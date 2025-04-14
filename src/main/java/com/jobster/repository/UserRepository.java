29190472b63fff022609778306bf2ffa8fdecb29
hariharen96 <hariharen1996@gmail.com>
2025-04-14 12:07:01 +0530

    feat:add repository layer for user Added UserRepository annotated with @Repository with custom query methods Added existsByUsername,existsByEmail,findByEmail methods

---
+++ src/main/java/com/jobster/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.jobster.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jobster.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+}
